832d66c38ba8142a9dca8c346775e72bef067ca4
Nikita Komarov <nikkom2518@gmail.com>
2022-02-20 19:02:38 +0400

    Add demo for Explorer-md

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-# Тестовое задание на Web-разработчика
+# Demo for Explorer-md
 
-Планировщик задач выполнен с помощью нативного JS, для более быстрой разработки основа css верстки сделана на W3.CSS.
-
-Для мобильных устрйоств и экранов уже 992px пропадает возможность перетаскивать задачи из backlog. 
-
-Demo - https://komarowski.github.io/index.html
-
-### Другие мои проекты
-
-- https://komarowski.site/ - мой сайт
-
-- https://adminii.ru/service - веб-приложение для онлайн записи
-- https://adminii.ru/lenagontar_studio - сайт одностраничник
-
-- https://adminii.ru/stomatology_template - сайт одностраничник
+This is demo for [Explorer-md](https://github.com/komarowski/Explorer-md) 
---
+++ assets/js/treeview-init.js
@@ -0,0 +1,23 @@
+﻿// const treeView = document.getElementById("tree-view");
+
+if (treeView && treeView != null){
+  const file = treeView.dataset.file;
+  const currentFile = document.getElementById(file);
+  if (currentFile != null) {
+    currentFile.classList.add("tree-view-item-current");
+    const indexes = [...file.matchAll(new RegExp('__', 'gi'))].map(a => a.index);
+    indexes.forEach(index => {
+      const folder = file.slice(0, index + 2);
+      let folderElement = document.getElementById(folder);
+      if (folderElement != null) {
+        folderElement.open = true;
+      }
+    });
+  } else {
+    const details = Array.from(treeView.querySelectorAll("details"))
+    details.forEach(element => {
+      element.open = true;
+    });
+  }
+}
+
